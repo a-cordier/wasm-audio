391e322a1468da78be997f8b735ad585ad34402c
Antoine Cordier <com.acordier@gmail.com>
2020-06-22 12:32:48 +0200

    :recycle: make known ranges const

--- src/worklets/range.cpp
+++ src/worklets/range.cpp
@@ -7,26 +7,26 @@ struct Range {
 	Range(const float min, const float max) :
 		min(min), max(max) {}
 
-	float map(float value, Range const &range) {
+	float map(float value, Range const &range) const {
 		return min + ((value - range.min) * (max - min) / (range.max - range.min));
 	}
 
-	float clamp(float value) {
+	float clamp(float value) const {
 		if (value < min) return min;
 		if (value > max) return max;
 		return value;
 	}
 };
 
-Range midiRange{ 0, 127.f };
-Range zeroOneRange{ 0.f, 1.f };
-Range attackRange{ 0.0001f, 1.f };
-Range decayRange{ 0.001f, 1.f };
-Range sustainRange{ 0.2f, 0.9f };
-Range releaseRange{ 0.2f, 1.f };
-Range cutoffRange{ 0.03f, .99f };
-Range resonanceRange{ 0.f, 0.97f };
-Range envelopeAmountRange{ 0.f, 1.f };
-Range semiShiftRange{ -24.f, 24.f };
-Range centShiftRange{ -50.f, 50.f };
-Range lfoFrequencyRange{ 0.1f, 25.f };
+const Range midiRange{ 0, 127.f };
+const Range zeroOneRange{ 0.f, 1.f };
+const Range attackRange{ 0.0001f, 1.f };
+const Range decayRange{ 0.001f, 1.f };
+const Range sustainRange{ 0.2f, 0.9f };
+const Range releaseRange{ 0.2f, 1.f };
+const Range cutoffRange{ 0.03f, .99f };
+const Range resonanceRange{ 0.f, 0.97f };
+const Range envelopeAmountRange{ 0.f, 1.f };
+const Range semiShiftRange{ -24.f, 24.f };
+const Range centShiftRange{ -50.f, 50.f };
+const Range lfoFrequencyRange{ 0.1f, 25.f };
